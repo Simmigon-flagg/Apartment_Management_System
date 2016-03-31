96b5e1c2b5e2581c45cf66f42bb1d468c0ff1467
Ryleichan <rchan5@student.gsu.edu>
2016-03-30 21:45:49 -0400

    Applicant and test file

---
+++ src/ApartmentSystem/Applicant.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ApartmentSystem;
+
+/**
+ *
+ * @author Riley
+ */
+public class Applicant {
+
+    private String phoneNumber;
+    private String address;
+
+    public Applicant(String phoneNumber, String address) {
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Applicant{" + "phoneNumber=" + phoneNumber + ", address=" + address + '}';
+    }
+}
---
+++ src/ApartmentSystem/TestApplicant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ApartmentSystem;
+
+/**
+ *
+ * @author Riley
+ */
+public class TestApplicant {
+    public static void main(String[] args) {
+  
+   Applicant testapplicant = new Applicant("6781234576", "1234 this address place");
+    System.out.println(testapplicant);
+    }
+}
+
