63627a033f0c4a757080b5ce23efd7ec5c004409
Simmigon <simmigon.flagg@gmail.com>
2016-04-05 01:15:06 -0400

    Hmmmm

--- src/ApartmentSystem/appHome.java
+++ src/ApartmentSystem/appHome.java
@@ -170,6 +170,8 @@ public class AppHome extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
